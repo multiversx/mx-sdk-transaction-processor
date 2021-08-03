e069f4ddc6fefb449191e5d1aecda34548939ca2
Traian Anghel <tanghel@live.com>
2021-08-03 12:32:48 +0300

    moved types back to single file

--- src/transaction.statistics.ts
+++
@@ -1,7 +0,0 @@
-export class TransactionStatistics {
-  secondsElapsed: number = 0;
-  processedNonces: number = 0;
-  noncesPerSecond: number = 0;
-  noncesLeft: number = 0;
-  secondsLeft: number = 0;
-}
