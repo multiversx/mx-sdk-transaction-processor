8c3cb71232a2d956cec9ccff8d1e289ece6d89bc
danielailie <danielailie12@gmail.com>
2023-01-10 10:30:35 +0200

    Update elrond to multiversx

--- example/crons/transaction.processor.service.ts
+++ example/crons/transaction.processor.service.ts
@@ -9,28 +9,30 @@ export class TransactionProcessorService {
   private readonly logger: Logger;
   private lastNonce: number | undefined;
   private readonly transactionProcessor = new TransactionProcessor();
-  constructor() {
+  constructor(
+  ) {
     this.logger = new Logger(TransactionProcessorService.name);
   }
 
   @Cron('*/1 * * * * *')
   async handleNewMultiversxTransactions() {
     Locker.lock('newMultiversxTransactions', async () => {
-      await this.transactionProcessor.start({
-        mode: Mode.ProcessByHyperblockTransactions,
-        gatewayUrl: 'https://gateway.multiversx.com', // mainnet
-        getLastProcessedNonce: async (_shardId: number, _currentNonce: number) => {
-          // In ProcessByHyperblockTransactions shardId will always be METACHAIN
-          return this.lastNonce;
-        },
-        setLastProcessedNonce: async (_shardId: number, nonce: number) => {
-          // In ProcessByHyperblockTransactions shardId will always be METACHAIN
-          this.lastNonce = nonce;
-        },
-        onTransactionsReceived: async (shardId: number, nonce: number, transactions: ShardTransaction[]) => {
-          console.log(`Received ${transactions.length} transactions on shard ${shardId} and nonce ${nonce}`);
-        },
-      });
+        await this.transactionProcessor.start({
+          mode: Mode.ProcessByHyperblockTransactions,
+          gatewayUrl: 'https://gateway.multiversx.com', // mainnet
+          getLastProcessedNonce: async (_shardId: number, _currentNonce: number) => {
+            // In ProcessByHyperblockTransactions shardId will always be METACHAIN
+            return this.lastNonce;
+          },
+          setLastProcessedNonce: async(_shardId: number, nonce: number) => {
+            // In ProcessByHyperblockTransactions shardId will always be METACHAIN
+            this.lastNonce = nonce;
+          },
+          onTransactionsReceived: async (shardId: number, nonce: number, transactions: ShardTransaction[]) => {
+            console.log(`Received ${transactions.length} transactions on shard ${shardId} and nonce ${nonce}`);
+          }
+        });
     });
   }
+
 }
